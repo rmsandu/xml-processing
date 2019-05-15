eb4199ae5c2b9c25ac1607e37a59f8e819237495
raluca-san <ralucam.sandu@gmail.com>
2019-05-15 10:02:59 +0200

    solve needle ire reference bug

--- XMLProcessing/D_extractIREAngles.py
+++ XMLProcessing/D_extractIREAngles.py
@@ -28,81 +28,70 @@ class ComputeAnglesTrajectories:
             PlannedTargetPoint = lesion_data['PlannedTargetPoint'].tolist()
             ValidationEntryPoint = lesion_data['ValidationEntryPoint'].tolist()
             ValidationTargetPoint = lesion_data['ValidationTargetPoint'].tolist()
-            NeedleType = lesion_data['NeedleType'].tolist()
             ReferenceNeedle = lesion_data['ReferenceNeedle'].tolist()
 
-            #%%
-            if True in ReferenceNeedle:
-                k = 0
+            ref_needle = False
+            for idx, val in enumerate(ReferenceNeedle):
+                if val is True:
+                    ref_needle = True
+            # if true reference needle --> k=0
+            # if all false reference needle --> k=1
+            # %%
+            if ref_needle is False:
+                k = 1
             else:
                 # if there is no reference needle for this trajectories, then start needle naming at 1
-                k = 1
+                k = 0
 
             for combination_angles in combinations(needles_lesion, 2):
 
-                try:
-                    if NeedleType[combination_angles[0]] == 'MWA' or NeedleType[combination_angles[1]] == 'MWA':
-                        continue  # go back to the beginning of the loop, else option not needed
-                except Exception as e:
-                    print(repr(e))
-
-                # try:
-                #     print(ReferenceNeedle[combination_angles[0]])
-                # except Exception as e:
-                #     print(repr(e))
-                #     continue
-                #
-                # try:
-                #     print(ReferenceNeedle[combination_angles[1]])
-                # except Exception as e:
-                #     print(repr(e))
-                #     continue
-
-                if ReferenceNeedle[combination_angles[0]] is False and ReferenceNeedle[combination_angles[1]] is False:
+                if ReferenceNeedle[combination_angles[0]-k] is False and ReferenceNeedle[combination_angles[1]-k] is False:
                     # no reference needle available, older version of XML CAS Logs
-                    needleA = needles_lesion[combination_angles[0]] + k
-                    needleB = needles_lesion[combination_angles[1]] + k
+                    needleA = needles_lesion[combination_angles[0]-k]
+                    needleB = needles_lesion[combination_angles[1]-k]
 
-                    if PlannedTargetPoint[combination_angles[0]].all() and PlannedTargetPoint[combination_angles[0]].all():
-                        angle_planned = AngleNeedles.angle_between(PlannedEntryPoint[combination_angles[0]],
-                                                                   PlannedTargetPoint[combination_angles[0]],
-                                                                   PlannedEntryPoint[combination_angles[1]],
-                                                                   PlannedTargetPoint[combination_angles[1]])
+                    if PlannedTargetPoint[combination_angles[0]].all() and PlannedTargetPoint[
+                            combination_angles[0]].all():
+                        angle_planned = AngleNeedles.angle_between(PlannedEntryPoint[combination_angles[0]-k],
+                                                                   PlannedTargetPoint[combination_angles[0]-k],
+                                                                   PlannedEntryPoint[combination_angles[1]-k],
+                                                                   PlannedTargetPoint[combination_angles[1]-k])
                     else:
                         angle_planned = np.nan
 
-                    if ValidationTargetPoint[combination_angles[0]] is not None \
-                            and ValidationTargetPoint[combination_angles[1]] is not None:
+                    if ValidationTargetPoint[combination_angles[0]-k] is not None \
+                            and ValidationTargetPoint[combination_angles[1]-k] is not None:
                         # if values exist for the validation then compute the validation angle
-                        angle_validation = AngleNeedles.angle_between(ValidationEntryPoint[combination_angles[0]],
-                                                                      ValidationTargetPoint[combination_angles[0]],
-                                                                      ValidationEntryPoint[combination_angles[1]],
-                                                                      ValidationTargetPoint[combination_angles[1]])
+                        angle_validation = AngleNeedles.angle_between(ValidationEntryPoint[combination_angles[0]-k],
+                                                                      ValidationTargetPoint[combination_angles[0]-k],
+                                                                      ValidationEntryPoint[combination_angles[1]-k],
+                                                                      ValidationTargetPoint[combination_angles[1]-k])
                     else:
                         # this angle pair hasn't been validated so assign NaN to the angle validation
                         angle_validation = np.nan
-                elif ReferenceNeedle[combination_angles[0]] is True:
+
+                elif ReferenceNeedle[combination_angles[0]-k] is True:
                     # ReferenceNeedle is never validated, only plan trajectories are available
                     needleA = 'Reference'
-                    needleB = needles_lesion[combination_angles[1]]
+                    needleB = needles_lesion[combination_angles[1]-k]
 
-                    if (PlannedTargetPoint[combination_angles[0]]) is not None \
-                            and (PlannedTargetPoint[combination_angles[0]]) is not None:
+                    if (PlannedTargetPoint[combination_angles[0]-k]) is not None \
+                            and (PlannedTargetPoint[combination_angles[0]-k]) is not None:
 
-                        angle_planned = AngleNeedles.angle_between(PlannedEntryPoint[combination_angles[0]],
-                                                                   PlannedTargetPoint[combination_angles[0]],
-                                                                   PlannedEntryPoint[combination_angles[1]],
-                                                                   PlannedTargetPoint[combination_angles[1]])
+                        angle_planned = AngleNeedles.angle_between(PlannedEntryPoint[combination_angles[0]-k],
+                                                                   PlannedTargetPoint[combination_angles[0]-k],
+                                                                   PlannedEntryPoint[combination_angles[1]-k],
+                                                                   PlannedTargetPoint[combination_angles[1]-k])
 
                     else:
                         angle_planned = np.nan
 
-                    if ValidationTargetPoint[combination_angles[1]] is not None:
+                    if ValidationTargetPoint[combination_angles[1]-k] is not None:
                         # if values exist for the validation then compute the validation angle
-                        angle_validation = AngleNeedles.angle_between(PlannedEntryPoint[combination_angles[0]],
-                                                                      PlannedTargetPoint[combination_angles[0]],
-                                                                      ValidationEntryPoint[combination_angles[1]],
-                                                                      ValidationTargetPoint[combination_angles[1]])
+                        angle_validation = AngleNeedles.angle_between(PlannedEntryPoint[combination_angles[0]-k],
+                                                                      PlannedTargetPoint[combination_angles[0]-k],
+                                                                      ValidationEntryPoint[combination_angles[1]-k],
+                                                                      ValidationTargetPoint[combination_angles[1]-k])
                     else:
                         angle_validation = np.nan
 
